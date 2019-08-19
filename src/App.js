312ac58e278712932248671ec9a773013f5e53a7
loek-finalist <loek.van.vark@finalist.nl>
2019-08-19 11:56:07 +0200

    add loek-spaceX href to react boilerplate app
    jsx class component App with escaped curly braces
    
    clean and create class based component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>Hello from react</h1>       
+//     </div>
+//   );
+// }
+
+class App extends Component {
+  render() {
+
+    let name = `Loek`;
+    
+    return (
+      <div className="App">
+        <h1>Hello from react!</h1>
+        <h2>Your name: {name}</h2>
+        <a href="https://loekvanvark.com/spacex-api/" target="_blank" label="loekvanvark {spaceX}">loek{`{spaceX}`}</a>
+      </div>
+    );
+  }
 }
 
 export default App;
