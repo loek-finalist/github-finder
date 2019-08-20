e494cdb94d6bfeb808154efb683164235a7d0c3b
loek-finalist <loek.van.vark@finalist.nl>
2019-08-20 15:06:21 +0200

    add search functionality with submit, 404 (App.js)

--- src/App.js
+++ src/App.js
@@ -13,13 +13,25 @@ class App extends Component {
     loading: false
   };
 
-  async componentDidMount() {
-    this.setState({ loading: true });
-    const response = await axios.get(`https://api.github.com/users?client_id=$
-      { process.env.REACT_APP_GITHUB_CLIENT_ID } & client_secret=$
-      { process.env.REACT_APP_GITHUB_CLIENT_SECRET }`);
+  // async componentDidMount() {
+  //   this.setState({ loading: true });
+  //   const response = await axios.get(`https://api.github.com/users?client_id=$
+  //     { process.env.REACT_APP_GITHUB_CLIENT_ID } & client_secret=$
+  //     { process.env.REACT_APP_GITHUB_CLIENT_SECRET }`);
 
-    this.setState({ users: response.data, loading: false });
+  //   this.setState({ users: response.data, loading: false });
+  // }
+
+  // Search Github Users
+  searchUsers = async text => {
+    this.setState({ loading: true});
+    console.log(text);
+    const response = await axios.get(`https://api.github.com/users/search?q=${text}&client_id=${
+      process.env.REACT_APP_GITHUB_CLIENT_ID
+    }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+
+    this.setState({ users: response.data.item, loading: false });
+
   }
 
   render() {
@@ -28,7 +40,7 @@ class App extends Component {
       <div className="App">
         <Navbar />
         <div className="container">
-          <Search />
+          <Search searchUsers={this.searchUsers} />
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,20 +1,28 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
 
 class Search extends Component {
 
     state = {
         text: ""
+    };
 
-    }
+    static propTypes = {
+        searchUsers: PropTypes.func.isRequired,
+        // clearUsers: PropTypes.func.isRequired,
+        // showClear: PropTypes.bool.isRequired
+    };
+
+    onSubmit = (e) => {
+        e.preventDefault();
+        this.props.searchUsers(this.state.text);
+        this.setState({ text: `` });
+    };
 
     onChange = (e) => {
         this.setState({ [e.target.name] : e.target.value });
-    }
-
-    onSubmit(e) {
-        e.preventDefault();
-        console.log(this.state.text);
-    } 
+    };
 
     render() {
         return (
@@ -34,7 +42,7 @@ class Search extends Component {
                 </form>
             </div>
         )
-    }
+    };
 }
 
 export default Search
