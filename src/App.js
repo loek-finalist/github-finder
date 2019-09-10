01714b909ff8f4c72fcf137b0b62dc022f44406b
loek-finalist <loek.van.vark@finalist.nl>
2019-09-10 09:39:55 +0200

    add route and link to About component/page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import React, { Component } from "react";
+import React, { Fragment, Component } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
 import Search from "./components/users/Search";
 import Alert from "./components/layout/Alert";
+import About from "./components/pages/About";
 import axios from "axios";
 
 import "./App.css";
@@ -14,15 +16,6 @@ class App extends Component {
     loading: false,
     alert: null
   };
-
-  // async componentDidMount() {
-  //   this.setState({ loading: true });
-  //   const response = await axios.get(`https://api.github.com/users?client_id=$
-  //     { process.env.REACT_APP_GITHUB_CLIENT_ID } & client_secret=$
-  //     { process.env.REACT_APP_GITHUB_CLIENT_SECRET }`);
-
-  //   this.setState({ users: response.data, loading: false });
-  // }
 
   // Search Github Users
   searchUsers = async text => {
@@ -38,13 +31,13 @@ class App extends Component {
   };
 
   // Clear users from search results (state)
-  clearUsers = () => this.setState( {users: [], loading: false} );
+  clearUsers = () => this.setState({ users: [], loading: false });
 
   // Set alert
   setAlert = (msg, type) => {
     this.setState({ alert: { msg: msg, type: type } });
-    
-    setTimeout(() => this.setState({ alert:null}), 4000 );
+
+    setTimeout(() => this.setState({ alert: null }), 4000);
   };
 
   render() {
@@ -52,19 +45,29 @@ class App extends Component {
     const { loading, users } = this.state;
 
     return (
-      <div className="App">
-        <Navbar />
-        <div className="container">
-          <Alert alert={this.state.alert} />
-          <Search 
-            searchUsers={this.searchUsers} 
-            clearUsers={this.clearUsers} 
-            showClear={this.state.users.length > 0 ? true : false}
-            setAlert={this.setAlert}
-            />
-          <Users loading={this.state.loading} users={this.state.users} />
+      <Router>
+        <div className="App">
+          <Navbar />
+          <div className="container">
+            <Alert alert={this.state.alert} />
+            <Switch>
+              <Route exact path="/" render={props => (
+                <Fragment>
+                  <Search
+                    searchUsers={this.searchUsers}
+                    clearUsers={this.clearUsers}
+                    showClear={this.state.users.length > 0 ? true : false}
+                    setAlert={this.setAlert}
+                  />
+                  <Users loading={this.state.loading} users={this.state.users} />
+                </Fragment>
+              )} />
+              <Route exact path="/about" component={About} />
+            </Switch>
+
+          </div>
         </div>
-      </div>
+      </Router>
     );
   };
 }
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 
 function Navbar({ icon, title }) {
@@ -8,6 +9,10 @@ function Navbar({ icon, title }) {
             <h1> 
                 <i className={icon} /> {title}
             </h1>
+            <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/about">About</Link></li>
+            </ul>
         </nav>
     );
 };
